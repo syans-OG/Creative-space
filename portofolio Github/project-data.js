5050d7db1d44eda4fb7c314fd735e57daca27bce
syans-OG <123starsquad@gmail.com>
2026-01-17 11:31:13 +0700

    Add files via upload

---
+++ portofolio Github/project-data.js
@@ -0,0 +1,108 @@
+const projectsData = {
+    "Pomodoro Timer": {
+        title: "Pomodoro Timer",
+        category: "Website",
+        brief: "Pomodoro Timer is a simple and effective time management tool that helps users stay focused and productive by breaking their work into focused intervals (typically 25 minutes), separated by short breaks.",
+        cardColor: "#db7a35a8",
+        roles: ["End-to-End Web Designer", "UI/UX Designer", "Frontend Developer"],
+        deliverables: ["Website Design", "Design System", "Interactive Prototype", "Frontend Implementation", "Tools Used"],
+        thumbnail: "asset/pomodoro/pomodoro_thumbnail.png",
+        featuredThumbnail: "asset/pomodoro/pomodoro_thumbnail2.png",
+        images: [
+            "asset/pomodoro/pomodoro.png",
+            "asset/pomodoro/pomodoro1.png",
+            "asset/pomodoro/pomodoro2.png",
+            "asset/pomodoro/pomodoro_mobile.png"
+        ],
+        imageCaptions: [
+            "Website",
+            "Main Layout",
+            "Themes Colors",
+            "Mobile View"
+        ]
+    },
+
+    "spendly-app": {
+        title: "Spendly",
+        category: "Mobile App",
+        brief: "Spendly is a personal finance mobile app that helps users track daily expenses, manage budgets, and understand their spending habits in a simple and intuitive way.",
+        cardColor: "#3a4cb3",
+        roles: ["Product Designer (End-to-End)", "UI/UX Design", "Frontend Development"],
+        deliverables: ["Design System", "Interactive Prototype", "Frontend Implementation"],
+        thumbnail: "asset/spendly/project_spendly.png",
+        featuredThumbnail: "asset/spendly/featured_work_spendly.png",
+        images: [
+            "asset/spendly/project_tagline_spendly.png",
+            "asset/spendly/project_details_spendly.png",
+        ],
+        imageCaptions: [
+            "App Tagline",
+            "App Details"
+        ]
+    },
+    "popcornriview-ui": {
+        title: "Popcorn Review",
+        category: "UI/UX Design",
+        brief: "PopcornReview is a movie review website that allows users to explore films, read and write reviews, and discover ratings in one place.",
+        cardColor: "#4a2c35",
+        roles: ["UI/UX Design", "Frontend Development"],
+        deliverables: ["Website UI Design", "Design System", "Interactive Prototype"],
+        thumbnail: "asset/popcorn/popcorn_logo.png",
+        featuredThumbnail: "asset/popcorn/popcorn_thumbnail.png",
+        images: [
+            "asset/popcorn/popcorn.png",
+            "asset/popcorn/popcorn1.png",
+            "asset/popcorn/popcorn2.png",
+            "asset/popcorn/popcorn3.png"
+        ],
+        imageCaptions: [
+            "UI/UX Design",
+            "Homepage",
+            "Trending page",
+            "Movie Details",
+        ]
+    },
+    "laundry-app": {
+        title: "Laundry Notes",
+        category: "Mobile App",
+        brief: "Laundry Notes is a simple mobile app that helps you record laundry items quickly and clearly without writing them one by one.",
+        cardColor: "#ff5745",
+        roles: ["Product Designer (End-to-End)", "UI/UX Design", "Frontend Development"],
+        deliverables: ["Design System", "Interactive Prototype", "Frontend Implementation"],
+        thumbnail: "asset/laundry/project_laundry.png",
+        featuredThumbnail: "asset/laundry/featured_work_laundry.png",
+        images: [
+            "asset/laundry/project_tagline_laundry.png",
+            "asset/laundry/project_detail_laundry.png"
+        ],
+        imageCaptions: [
+            "App Tagline",
+            "App Features"
+        ]
+    },
+    "truman-site": {
+        title: "Truman Site",
+        category: "Website",
+        brief: "The Truman Show website is an interactive web experience inspired by the movie The Truman Show.The website presents story elements, characters, and trivia through dynamic navigation and interactive features, allowing users to explore the world of Truman in an engaging and immersive way.",
+        cardColor: "#2C3E50",
+        roles: ["End-to-End Web Designer", "UI/UX Designer", "Frontend Developer"],
+        deliverables: ["Website UI/UX Design", "Design System", "Interactive Prototype", "Frontend Implementation"],
+        thumbnail: "asset/truman/truman_logo.png",
+        featuredThumbnail: "asset/truman/truman_thumbnail.png",
+        images: [
+            "asset/truman/truman.png",
+            "asset/truman/truman1.png",
+            "asset/truman/truman2.png",
+            "asset/truman/truman3.png",
+            "asset/truman/truman_mobile.png"
+        ],
+
+        imageCaptions: [
+            "landing page website",
+            "Main Layout",
+            "Themes Details",
+            "Character Details",
+            "Mobile View"
+        ]
+    },
+};
